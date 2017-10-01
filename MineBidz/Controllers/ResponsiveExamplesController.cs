2013c65140daffc28a9c6297bc0b3e44bfaded49
Serguei Razykov <serguei.razykov@gmail.com>
2017-09-30 23:57:37 -0400

    Commit example and first changes to layout

---
+++ MineBidz/Controllers/ResponsiveExamplesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MineBidz.Controllers
+{
+    public class ResponsiveExamplesController : Controller
+    {
+
+        public ActionResult W3SiteTemplate()
+        {
+            return View();
+        }
+
+    }
+}
